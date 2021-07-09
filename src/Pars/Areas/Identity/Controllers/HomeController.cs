f85635c85d7da36c28f290a605850a7bc2f7ca14
miladmazaheri <milad.kbr@outlook.com>
2021-07-09 16:57:03 +0430

    1

--- src/Pars/Areas/Identity/Controllers/HomeController.cs
+++ src/Pars/Areas/Identity/Controllers/HomeController.cs
@@ -6,10 +6,10 @@ namespace Pars.Areas.Identity.Controllers
 {
     [Area(AreaConstants.IdentityArea)]
     [Authorize]
-    [BreadCrumb(Title = "خانه", UseDefaultRouteUrl = true, Order = 0)]
+    //[BreadCrumb(Title = "خانه", UseDefaultRouteUrl = true, Order = 0)]
     public class HomeController : Controller
     {
-        [BreadCrumb(Title = "ایندکس", Order = 1)]
+        //[BreadCrumb(Title = "داشبورد", Order = 1)]
         public IActionResult Index() => View();
     }
 }
